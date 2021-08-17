d34f9189abe663931f537c81f8bed3679da9edc3
Thomas Preston <thomasmarkpreston@gmail.com>
2021-08-17 13:24:42 +0100

    sysinfo_server: Add docs and tidy output

--- sysinfo_server.go
+++ sysinfo_server.go
@@ -1,3 +1,4 @@
+// sysinfo_server serves a REST API of `systemd-analyze time`
 package main
 
 import (
@@ -14,32 +15,37 @@ import (
 const systemdAnalyze string = "/usr/bin/systemd-analyze"
 const version int = 1
 
-var handlers = map[string]func(http.ResponseWriter, *http.Request){
+type handlerMap map[string]func(http.ResponseWriter, *http.Request)
+
+var handlers = handlerMap{
 	"/":         handleRoot,
 	"/version":  handleVersion,
 	"/duration": handleDuration,
 }
 
-// Handle the root request (/) by just running systemd-analyze
-func handleRoot(w http.ResponseWriter, r *http.Request) {
-	var out bytes.Buffer
+// writeError writes a 500 Internal Server Error
+func writeError(w http.ResponseWriter, err error) {
+	log.Println(fmt.Sprintf("Error %v", err))
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+}
 
+// handleRoot handles the root request (/) by just running systemd-analyze
+func handleRoot(w http.ResponseWriter, r *http.Request) {
 	cmd := exec.Command(systemdAnalyze)
-	cmd.Stdout = &out
+	cmd.Stdout = w
 	if err := cmd.Run(); err != nil {
 		writeError(w, err)
 		return
 	}
-
-	fmt.Fprintf(w, "%v", out.String())
 }
 
-// Handle the version request
+// handleVersion handles the version request
 func handleVersion(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%v", version)
 }
 
-// Handle the duration request
+// handleDuration handles the duration request
 func handleDuration(w http.ResponseWriter, r *http.Request) {
 	var out bytes.Buffer
 
@@ -69,22 +75,19 @@ func handleDuration(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%v", total)
 }
 
-// Write a 500 Internal Server Error
-func writeError(w http.ResponseWriter, err error) {
-	log.Println(fmt.Sprintf("Error %v", err))
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte("500 Internal Server Error"))
+// printReady prints that the server is ready and all available endpoints
+func printReady(handlers handlerMap) {
+	endpoints := make([]string, 0, len(handlers))
+	for e := range handlers {
+		endpoints = append(endpoints, e)
+	}
+	fmt.Printf("Server ready, endpoints: %v\n", strings.Join(endpoints, ", "))
 }
 
 func main() {
 	for handler, handler_fn := range handlers {
 		http.HandleFunc(handler, handler_fn)
 	}
-
-	fmt.Println("Server ready, endpoints:")
-	for handler, _ := range handlers {
-		fmt.Println(handler)
-	}
-
+	printReady(handlers)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
